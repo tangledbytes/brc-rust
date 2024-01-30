9baa72cb261deb4a4b083e0e34410191ad2d2af0
Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>
2024-01-31 01:10:09 +0530

    iteration 3
    Signed-off-by: Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>
    
    iteration 3 - fix new line read
    
    Signed-off-by: Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,22 @@
-use std::{collections::HashMap, env, io::{BufRead, BufReader}};
+use std::{
+    collections::HashMap,
+    env,
+    ffi::{c_int, c_void},
+    fs,
+    os::fd::AsRawFd,
+    slice,
+};
+
+extern "C" {
+    pub fn mmap(
+        addr: *mut c_void,
+        len: u64,
+        prot: c_int,
+        flags: c_int,
+        fd: c_int,
+        offset: u64,
+    ) -> *mut c_void;
+}
 
 #[derive(Debug)]
 struct Data {
@@ -8,7 +26,31 @@ struct Data {
     count: u32,
 }
 
-fn print_store(sorted_store: &Vec<(String, Data)>) {
+fn load_file(filename: &str) -> &'static [u8] {
+    const PROT_READ: i32 = 0x1;
+    const MAP_PRIVATE: i32 = 0x2;
+
+    let file = fs::File::open(filename).expect("failed to open file");
+    let size = file
+        .metadata()
+        .expect("failed to get metadata of the file")
+        .len();
+
+    let res = unsafe {
+        mmap(
+            core::ptr::null_mut(),
+            size,
+            PROT_READ,
+            MAP_PRIVATE,
+            file.as_raw_fd(),
+            0,
+        )
+    };
+
+    unsafe { slice::from_raw_parts(res as *const _ as *const u8, size as _) }
+}
+
+fn print_store(sorted_store: &Vec<(&str, Data)>) {
     print!("{{");
 
     for (idx, val) in sorted_store.iter().enumerate() {
@@ -28,14 +70,15 @@ fn print_store(sorted_store: &Vec<(String, Data)>) {
 }
 
 fn main() {
-    let path = env::args().nth(1).expect("Usage: <bin> <path-to-measurements.txt>");
-    let mut store: HashMap<String, Data> = HashMap::new();
+    let path = env::args()
+        .nth(1)
+        .expect("Usage: <bin> <path-to-measurements.txt>");
+    let mut store: HashMap<&'static str, Data> = HashMap::new();
 
-    let file = std::fs::File::open(path).unwrap();
-    let reader = BufReader::new(file);
+    let data = load_file(&path);
 
-    for line in reader.lines().map_while(Result::ok) {
-        let (name, val) = line.split_once(';').unwrap();
+    for line in std::str::from_utf8(data).unwrap().split('\n') {
+        let (name, val) = line.trim().split_once(';').unwrap();
         let val = val.parse::<f32>().unwrap();
 
         if let Some(data) = store.get_mut(name) {
@@ -45,17 +88,20 @@ fn main() {
             data.sum += val;
             data.count += 1;
         } else {
-            store.insert(name.to_string(), Data {
-                min: val,
-                max: val,
-                sum: val,
-                count: 1,
-            });
+            store.insert(
+                name,
+                Data {
+                    min: val,
+                    max: val,
+                    sum: val,
+                    count: 1,
+                },
+            );
         }
     }
 
     let mut v = store.into_iter().collect::<Vec<_>>();
-    v.sort_unstable_by(|a, b| a.0.cmp(&b.0));
+    v.sort_unstable_by_key(|p| p.0);
 
     print_store(&v);
 }
